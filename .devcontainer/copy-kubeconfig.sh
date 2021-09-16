c0e67c1560a4517f1ecf376583c90b95b6a79ab4
Tristan Schrader <9901294+schradert@users.noreply.github.com>
2021-09-16 06:36:46 +0000

    devcontainer dockerfile redesign to support building airflow dags in python within devcontainer, as well as work on the local k3d cluster from within the devcontainer that's not part of the cluster. The docker socket was provided as well as the kubeconfig directory, and SYNC_LOCALHOST_KUBECONFIG env variable to support syncing changes. PATH and PROJECT are also included for general convenience (also the PATH extension was moved here from the dockerfile for consistency). KUBECONFIG copy script is included as well and to be called from dockerfile during build

---
+++ .devcontainer/copy-kubeconfig.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -i
+
+# Copies ~/.kube/config file into the container and swap out localhost
+# for host.docker.internal whenever a new shell starts to keep them in sync.
+KUBE_LOCAL=/usr/local/share/kube-localhost
+KUBE=$HOME/.kube
+KUBECONFIG=$KUBE/config
+if [[ $SYNC_LOCALHOST_KUBECONFIG = "true" ]] && [[ -d $KUBE_LOCAL ]]; then
+  mkdir -p $KUBE
+  sudo cp -r $KUBE_LOCAL/* $KUBE
+  sudo chown -R $(id -u) $KUBE
+  sed -i -e "s/localhost/host.docker.internal/g" $KUBECONFIG
+  sed -i -e "s/127.0.0.1/host.docker.internal/g" $KUBECONFIG
+fi
