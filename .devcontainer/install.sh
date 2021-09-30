01a4a2d148fba2c4bba9acf1461d7b6b4f2060bf
Tristan Schrader <9901294+schradert@users.noreply.github.com>
2021-09-30 03:29:51 +0000

    Moved main installation script of devcontainer dockerfile to separate file so that line breaks in layers weren't confusing as to what would break the line in the dockerfile versus on the line of the command itself. Now kubernetes api and config are actually shared with devcontainer. We can run prettier on shell scripts now too with npm global binaries (much easier to work with than yarn).

---
+++ .devcontainer/install.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Install Node
+apk add --no-cache nodejs yarn npm
+
+# Install Prettier for formatting
+npm i -g prettier prettier-plugin-sh
+
+# Install Python and dependencies to support Airflow development
+apk add --no-cache \
+  gcc musl-dev openssl-dev libffi-dev cargo \
+  python3 python3-dev py-pip py3-pandas
+ln -s python3.9 /usr/bin/python
+pip install --upgrade pip
+pip install apache-airflow
+
+# Install Java & Scala
+apk add --no-cache openjdk11
+SBT=https://github.com/sbt/sbt/releases/download/v1.5.5/sbt-1.5.5.tgz
+curl -fsSL $SBT | tar xvz -C /usr/lib
+ln -s /usr/lib/sbt/bin/sbt /usr/bin/sbt
+
+# Install kubectl
+KUBECTL_URI=https://storage.googleapis.com/kubernetes-release/release/
+KUBECTL_VER=$(curl -s $KUBE_RELEASE_URI/stable.txt)
+KUBECTL=/usr/local/bin/kubectl
+curl -sSL -o $KUBECTL $KUBECTL_URI/$KUBECTL_VER/bin/linux/amd64/kubectl
+chmod +x $KUBECTL
+
+# Install Helm
+HELM=https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+curl -s $HELM | bash -
