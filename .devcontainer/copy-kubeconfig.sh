2535b106426569c2424ecf9482b30db3aa919bd9
Tristan Schrader <9901294+schradert@users.noreply.github.com>
2021-09-30 05:14:39 +0000

    Completed kubernetes integration by replacing $HOME with /home/vscode (for some reason it isn't defined during build) and adding an additional file edit line for the kubeconfig file, specifically including 0.0.0.0 in the list of IP addresses to be substituted for host.docker.internal in the passed kubeconfig.

--- .devcontainer/copy-kubeconfig.sh
+++ .devcontainer/copy-kubeconfig.sh
@@ -3,7 +3,7 @@
 # Copies ~/.kube/config file into the container and swap out localhost
 # for host.docker.internal whenever a new shell starts to keep them in sync.
 KUBE_LOCAL=/usr/local/share/kube-localhost
-KUBE=$HOME/.kube
+KUBE=/home/vscode/.kube
 KUBECONFIG=$KUBE/config
 if [[ $SYNC_LOCALHOST_KUBECONFIG = "true" ]] && [[ -d $KUBE_LOCAL ]]; then
   mkdir -p $KUBE
@@ -11,4 +11,5 @@ if [[ $SYNC_LOCALHOST_KUBECONFIG = "true" ]] && [[ -d $KUBE_LOCAL ]]; then
   sudo chown -R $(id -u) $KUBE
   sed -i -e "s/localhost/host.docker.internal/g" $KUBECONFIG
   sed -i -e "s/127.0.0.1/host.docker.internal/g" $KUBECONFIG
+  sed -i -e "s/0.0.0.0/host.docker.internal/g" $KUBECONFIG
 fi
